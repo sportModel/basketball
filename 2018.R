8c2753861a607a904299d67150d6d88ccc25f9fc
pat5547 <pat5547@gmail.com>
2018-01-25 07:29:48 -0600

    More Iowa 2018 cbb

--- 2018.R
+++ 2018.R
@@ -1,2 +1,19 @@
 # Iowa
-ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-23-19-iowa.html')
+ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-23-19-iowa.html') # Wisconsin
+ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-20-12-iowa.html') # Purdue
+ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-17-19-rutgers.html')
+ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-11-20-illinois.html')
+ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-07-20-maryland.html')
+ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-04-19-iowa.html') # Ohio St
+ncaag('https://www.sports-reference.com/cbb/boxscores/2018-01-02-19-iowa.html') # Michigan
+ncaag('https://www.sports-reference.com/cbb/boxscores/2017-12-29-20-iowa.html') # N Illinois
+ncaag('https://www.sports-reference.com/cbb/boxscores/2017-12-22-21-iowa.html') # Colorado
+ncaag('') #
+ncaag('') #
+ncaag('') #
+ncaag('') #
+ncaag('') #
+ncaag('') #
+ncaag('') #
+ncaag('') #
+ncaag('') #

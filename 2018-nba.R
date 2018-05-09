42cd3af411fb361b8621f28e44f7e0aaee87c578
pat5547 <pat5547@gmail.com>
2018-05-09 08:28:34 -0500

    nba game import working now (fixed formatting)

---
+++ 2018-nba.R
@@ -0,0 +1,6 @@
+# Phi v Bos
+nbag('https://www.basketball-reference.com/boxscores/201804300BOS.html')
+nbag('https://www.basketball-reference.com/boxscores/201805030BOS.html')
+nbag('https://www.basketball-reference.com/boxscores/201805050PHI.html')
+nbag('https://www.basketball-reference.com/boxscores/201805070PHI.html')
+
--- functions/games/nbagFormat.R
+++ functions/games/nbagFormat.R
@@ -13,9 +13,15 @@ nbagFormat <- function(url) {
   val
 }
 nbagFormatTeam <- function(team, tname) {
-  team <- team[team$Starters!="Reserves" & team$MP != "Did Not Play",]
+  team <- team[team$Starters!="Reserves" & !(team$MP %in% c("Did Not Play", "Did Not Dress")),]
   for (j in 3:ncol(team)) {
     team[,j] <- as.numeric(team[,j])
   }
+  team$MP <- fixMP(team$MP)
   val <- data.frame(Name=team$Starters, Team=tname, Pos='', pg=.2, sg=.2, sf=.2, pf=.2, c=.2, No=NA, Yr=NA, Ht=NA, Wt=NA, G=1, team[,-1], check.names=FALSE)
+}
+fixMP <- function(x) {
+  A <- stringr::str_split(x, ':', simplify=TRUE)
+  mp <- as.numeric(A[,1]) + as.numeric(A[,2])/60
+  return(round(mp, 1))
 }

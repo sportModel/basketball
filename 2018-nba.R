d521d1ef40b9eb937ea71db9be116485f1f89bb4
pat5547 <pat5547@gmail.com>
2018-05-10 06:10:04 -0500

    NBA 2018

--- 2018-nba.R
+++ 2018-nba.R
@@ -4,3 +4,9 @@ nbag('https://www.basketball-reference.com/boxscores/201805030BOS.html')
 nbag('https://www.basketball-reference.com/boxscores/201805050PHI.html')
 nbag('https://www.basketball-reference.com/boxscores/201805070PHI.html')
 
+# Uta v Hou
+nbag('https://www.basketball-reference.com/boxscores/201804290HOU.html')
+nbag('https://www.basketball-reference.com/boxscores/201805020HOU.html')
+nbag('https://www.basketball-reference.com/boxscores/201805040UTA.html')
+nbag('https://www.basketball-reference.com/boxscores/201805060UTA.html')
+nbag('https://www.basketball-reference.com/boxscores/201805080HOU.html')

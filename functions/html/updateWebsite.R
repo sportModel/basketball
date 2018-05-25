0b0b301cb44a22f3db61b9998d36ee108267d596
pat5547 <pat5547@gmail.com>
2018-05-25 05:50:04 -0500

    Updates in progress for nba: works (except web), but needs to be standardized

--- functions/html/updateWebsite.R
+++ functions/html/updateWebsite.R
@@ -1,8 +1,8 @@
 updateWebsite <- function(raw, tf, vc, team.raw) {
-  if (par@level=="nba") team.raw <- raw$Team.Opp
-  makePos(vc, team.raw)
+  #if (par@level=="nba") team.raw <- raw$Team.Opp
+  #makePos(vc, team.raw)
   if (par@level=="nba") makeTeams.nba(raw)
   if (par@level=="ncaa") makeTeams.ncaa(raw, team.raw)
   makeTeam(raw, tf, vc, team.raw)
-  makeTeamComparison(team.raw)
+  #makeTeamComparison(team.raw)
 }
--- nba.R
+++ nba.R
@@ -3,5 +3,14 @@ nba.raw <- formatNBAdata()
 # FIX THIS SO IT RETURNS A TEAM DT ALSO
 nba.tf <- calcTF(nba.raw$Totals, nba.raw$Team.Opp$G)
 nba.vc <- calcVC(nba.tf, nba.raw$Team.Opp)
-save(nba.raw, nba.tf, nba.vc, file=paste("data/nba/2018/nba2018.rda"))
+save(nba.raw, nba.tf, nba.vc, file="data/nba/2018/nba2018.rda")
 updateWebsite(nba.raw, nba.tf, nba.vc)
+
+load('data/nba/2018/nba2018.rda')
+VC <- nba.vc
+VC[,grep('VC', names(VC))] <- round(VC[,grep('VC', names(VC))], 1)
+VC <- data.table(VC)
+VC[Team=="cle", c(2, 3, 10:26)][order(-WC)]
+VC[Team=="bos", c(2, 3, 10:26)][order(-WC)]
+VC[Team=="gsw", c(2, 3, 10:26)][order(-WC)]
+VC[Team=="hou", c(2, 3, 10:26)][order(-WC)]

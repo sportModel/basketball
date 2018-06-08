0e87eafaffdb450a68276276819a0126d773ee6c
pat5547 <pat5547@gmail.com>
2018-06-08 06:13:24 -0500

    Bringing NCAA team formatting up to date (consistent with NBA)

--- functions/data/formatNCAAteam.R
+++ functions/data/formatNCAAteam.R
@@ -4,12 +4,26 @@ formatNCAAteam <- function(team) {
   rec <- raw[grep('Overall', raw)][1]
   rec <- gsub(".* (\\d+-\\d+) .*", "\\1", rec)
   rec <- as.numeric(strsplit(rec, '-')[[1]])
+  w <- rec[1]
+  l <- rec[2]
 
   raw <- readHTMLTable(filename)
-  val2 <- raw$team_stats[c(1,3), c("FG","FGA","FT","FTA","3P","3PA","PTS","ORB","DRB","AST","STL","TOV","BLK")]
+  tmp <- raw$team_stats[c(1,3), c("FG","FGA","FT","FTA","3P","3PA","PTS","ORB","DRB","AST","STL","TOV","BLK")]
 
-  tmp <- cbind(G=raw$team_stats[c(1,3), "G"], W=rec, L=rev(rec), val2)
-  val <- as.numeric(c(tmp[1,],tmp[2,]))
-  names(val) <- c(paste0("Tm", colnames(tmp)), paste0("Op", colnames(tmp)))
-  val
+  for (j in 1:ncol(tmp)) tmp[,j] <- as.numeric(tmp[,j])
+  Tm <- tmp[1,]
+  Op <- tmp[2,]
+  names(Tm) <- paste0("Tm", colnames(Tm))
+  names(Op) <- paste0("Op", colnames(Op))
+  out <- data.table(Team=team, G=as.numeric(raw$team_stats[1, "G"]), W=w, L=l, Tm, Op)
+  out[, TmORBPct := TmORB/(TmORB+OpDRB)]
+  out[, OpORBPct := OpORB/(OpORB+TmDRB)]
+  out[, TmAstRate := TmAST/TmFG]
+  out[, OpAstRate := OpAST/OpFG]
+  out[, TmMiss := TmFGA - TmFG]
+  out[, OpMiss := OpFGA - OpFG]
+  TmPoss <- out$TmFGA-out$TmORBPct*(out$TmFGA-out$TmFG)*par@constants[1]+out$TmTOV+out$TmFTA*par@constants[2]
+  OpPoss <- out$OpFGA-out$OpORBPct*(out$OpFGA-out$OpFG)*par@constants[1]+out$OpTOV+out$OpFTA*par@constants[2]
+  out[, Poss := (TmPoss+OpPoss)/2/G]
+  out
 }
--- functions/data/formatNCAAteamData.R
+++ functions/data/formatNCAAteamData.R
@@ -1,15 +1,8 @@
 formatNCAAteamData <- function() {
-  team.raw <- NULL
-  for (i in par@team) {
-    team.i <- formatNCAAteam(i)
-    team.raw <- rbind(team.raw, team.i)
+  out <- NULL
+  for (team in par@team) {
+    out <- rbind(out, formatNCAAteam(team))
   }
-  rownames(team.raw) <- par@team
-  TmORBPct <- team.raw[,"TmORB"]/(team.raw[,"TmORB"]+team.raw[,"OpDRB"])
-  OpORBPct <- team.raw[,"OpORB"]/(team.raw[,"OpORB"]+team.raw[,"TmDRB"])
-  TmPoss <- team.raw[,"TmFGA"]-TmORBPct*(team.raw[,"TmFGA"]-team.raw[,"TmFG"])*par@constants[1]+team.raw[,"TmTOV"]+team.raw[,"TmFTA"]*par@constants[2]
-  OpPoss <- team.raw[,"OpFGA"]-OpORBPct*(team.raw[,"OpFGA"]-team.raw[,"OpFG"])*par@constants[1]+team.raw[,"OpTOV"]+team.raw[,"OpFTA"]*par@constants[2]
-  team.raw <- as.data.frame(team.raw)
-  team.raw$Poss <- (TmPoss+OpPoss)/2/team.raw[,"TmG"]
-  team.raw
+  setkey(out, Team)
+  out
 }
